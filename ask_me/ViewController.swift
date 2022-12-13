78242f23f79ed47de9133432624d47de9a4f7ff1
Roman <ar.renanv@gmail.com>
2022-12-13 17:52:22 +0500

    added functionality of ask button, added app icon and other images

--- ask_me/ViewController.swift
+++ ask_me/ViewController.swift
@@ -1,14 +1,17 @@
-//
-//  ViewController.swift
-//  ask_me
-//
-//  Created by Роман Архиреев on 13/12/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var answerView: UIImageView!
+    
+    @IBAction func askButtton(_ sender: UIButton) {
+        let answers = [#imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball4")]
+        
+        answerView.image = answers.randomElement()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
